2d39c7d76544a32c7615d7fedc311fac73d80dee
Robert Zieba <robertzieba@microsoft.com>
2025-02-03 10:59:47 -0700

    embedded-service: Add power module

--- embedded-service/src/lib.rs
+++ embedded-service/src/lib.rs
@@ -12,6 +12,7 @@ pub mod buffer;
 pub mod comms;
 pub mod fmt;
 pub mod hid;
+pub mod power;
 pub mod type_c;
 
 /// initialize all service static interfaces as required. Ideally, this is done before subsystem initialization
