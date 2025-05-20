3f187a9b915ff5e87b24d3128a6d50d057e8d357
Robert Zieba <robertzieba@microsoft.com>
2025-05-20 14:53:48 -0600

    examples/rt685: Add type-C cfu update example

---
+++ examples/rt685s-evk/src/bin/type_c_cfu.rs
@@ -0,0 +1,185 @@
+#![no_std]
+#![no_main]
+
+use ::tps6699x::ADDR1;
+use defmt::info;
+use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
+use embassy_executor::Spawner;
+use embassy_imxrt::gpio::{Input, Inverter, Pull};
+use embassy_imxrt::i2c::master::{Config, I2cMaster};
+use embassy_imxrt::i2c::Async;
+use embassy_imxrt::{bind_interrupts, peripherals};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::mutex::Mutex;
+use embassy_sync::once_lock::OnceLock;
+use embassy_time::{self as _, Delay};
+use static_cell::StaticCell;
+use embedded_services::cfu::component::RequestData;
+use embedded_cfu_protocol::protocol_definitions::*;
+use embedded_services::cfu::component::InternalResponseData;
+use embedded_services::cfu;
+use embedded_services::power::policy::DeviceId as PowerId;
+use embedded_services::type_c::{self, ControllerId};
+use embedded_usb_pd::GlobalPortId;
+use embassy_time::Timer;
+use tps6699x::asynchronous::embassy as tps6699x;
+use type_c_service::driver::tps6699x::{self as tps6699x_drv};
+
+extern crate rt685s_evk_example;
+
+bind_interrupts!(struct Irqs {
+    FLEXCOMM2 => embassy_imxrt::i2c::InterruptHandler<peripherals::FLEXCOMM2>;
+});
+
+type BusMaster<'a> = I2cMaster<'a, Async>;
+type BusDevice<'a> = I2cDevice<'a, NoopRawMutex, BusMaster<'a>>;
+type Wrapper<'a> = tps6699x_drv::Tps66994Wrapper<'a, NoopRawMutex, BusDevice<'a>>;
+type Controller<'a> = tps6699x::controller::Controller<NoopRawMutex, BusDevice<'a>>;
+type Interrupt<'a> = tps6699x::Interrupt<'a, NoopRawMutex, BusDevice<'a>>;
+
+const CONTROLLER0_ID: ControllerId = ControllerId(0);
+const CONTROLLER0_CFU_ID: ComponentId = 0;
+const PORT0_ID: GlobalPortId = GlobalPortId(0);
+const PORT1_ID: GlobalPortId = GlobalPortId(1);
+const PORT0_PWR_ID: PowerId = PowerId(0);
+const PORT1_PWR_ID: PowerId = PowerId(1);
+
+#[embassy_executor::task]
+async fn pd_controller_task(controller: &'static Wrapper<'static>) {
+    loop {
+        controller.process().await;
+    }
+}
+
+#[embassy_executor::task]
+async fn interrupt_task(mut int_in: Input<'static>, mut interrupt: Interrupt<'static>) {
+    tps6699x::task::interrupt_task(&mut int_in, &mut [&mut interrupt]).await;
+}
+
+#[embassy_executor::task]
+async fn fw_update_task() {
+    Timer::after_millis(1000).await;
+    let context = cfu::ContextToken::create().unwrap();
+    let device = context.get_device(CONTROLLER0_CFU_ID).await.unwrap();
+
+    info!("Getting FW version");
+    let response = device.execute_device_request(RequestData::FwVersionRequest).await.unwrap();
+    let prev_version = match response {
+        InternalResponseData::FwVersionResponse(GetFwVersionResponse { component_info, .. }) => {
+            Into::<u32>::into(component_info[0].fw_version)
+        }
+        _ => panic!("Unexpected response"),
+    };
+    info!("Got version: {:#x}", prev_version);
+
+    info!("Giving offer");
+    let offer = device.execute_device_request(RequestData::GiveOffer(FwUpdateOffer::new(HostToken::Driver, CONTROLLER0_CFU_ID, FwVersion::default(), 0, 0))).await.unwrap();
+    info!("Got response: {:?}", offer);
+
+    let fw = include_bytes!("../../TPS66994_Host.bin");
+    let num_chunks = fw.len() / DEFAULT_DATA_LENGTH;
+
+    for (i, chunk) in fw.chunks(DEFAULT_DATA_LENGTH).enumerate() {
+        let header = FwUpdateContentHeader {
+            data_length: chunk.len() as u8,
+            sequence_num: i as u16,
+            firmware_address: (i * DEFAULT_DATA_LENGTH) as u32,
+            flags: if i == 0 {
+                FW_UPDATE_FLAG_FIRST_BLOCK
+            } else if i == num_chunks - 1 {
+                FW_UPDATE_FLAG_LAST_BLOCK
+            } else {
+                0
+            }
+        };
+        
+        let mut chunk_data = [0u8; DEFAULT_DATA_LENGTH];
+        chunk_data[..chunk.len()].copy_from_slice(chunk);
+        let request = FwUpdateContentCommand {
+            header,
+            data: chunk_data,
+        };
+
+        info!("Sending chunk {} of {}", i, fw.len());
+        let response = device.execute_device_request(RequestData::GiveContent(request)).await.unwrap();
+        info!("Got response: {:?}", response);
+    }
+
+    device.execute_device_request(RequestData::FinalizeUpdate).await.unwrap();
+
+    Timer::after_millis(2000).await;
+    info!("Getting FW version");
+    let response = device.execute_device_request(RequestData::FwVersionRequest).await.unwrap();
+    let version = match response {
+        InternalResponseData::FwVersionResponse(GetFwVersionResponse { component_info, .. }) => {
+            Into::<u32>::into(component_info[0].fw_version)
+        }
+        _ => panic!("Unexpected response"),
+    };
+    info!("Got previous version: {:#x}", prev_version);
+    info!("Got version: {:#x}",version);
+}
+
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_imxrt::init(Default::default());
+
+    info!("Embedded service init");
+    embedded_services::init().await;
+
+    type_c::controller::init();
+
+    info!("Spawining power policy task");
+    spawner.must_spawn(power_policy_service::task(Default::default()));
+
+    info!("Spawining type-c service task");
+    spawner.must_spawn(type_c_service::task());
+
+    let int_in = Input::new(p.PIO1_7, Pull::Up, Inverter::Disabled);
+    static BUS: OnceLock<Mutex<NoopRawMutex, BusMaster<'static>>> = OnceLock::new();
+    let bus = BUS.get_or_init(|| {
+        Mutex::new(
+            I2cMaster::new_async(p.FLEXCOMM2, p.PIO0_18, p.PIO0_17, Irqs, Config::default(), p.DMA0_CH5).unwrap(),
+        )
+    });
+
+    let device = I2cDevice::new(bus);
+
+    static CONTROLLER: StaticCell<Controller<'static>> = StaticCell::new();
+    let controller = CONTROLLER.init(Controller::new_tps66994(device, ADDR1).unwrap());
+    let (mut tps6699x, interrupt) = controller.make_parts();
+
+    info!("Resetting PD controller");
+    let mut delay = Delay;
+    tps6699x.reset(&mut delay).await.unwrap();
+
+    info!("Spawining interrupt task");
+    spawner.must_spawn(interrupt_task(int_in, interrupt));
+
+    // These aren't enabled by default
+    tps6699x
+        .modify_interrupt_mask_all(|mask| {
+            mask.set_am_entered(true);
+            mask.set_dp_sid_status_updated(true);
+            mask.set_intel_vid_status_updated(true);
+            mask.set_usb_status_updated(true);
+            mask.set_power_path_switch_changed(true);
+            *mask
+        })
+        .await
+        .unwrap();
+
+    static PD_PORTS: [GlobalPortId; 2] = [PORT0_ID, PORT1_ID];
+
+    info!("Spawining PD controller task");
+    static PD_CONTROLLER: OnceLock<Wrapper> = OnceLock::new();
+    let pd_controller = PD_CONTROLLER.get_or_init(|| {
+        tps6699x_drv::tps66994(tps6699x, CONTROLLER0_ID, &PD_PORTS, [PORT0_PWR_ID, PORT1_PWR_ID], CONTROLLER0_CFU_ID).unwrap()
+    });
+
+    pd_controller.register().await.unwrap();
+    spawner.must_spawn(pd_controller_task(pd_controller));
+
+    spawner.must_spawn(fw_update_task());
+}
---
+++ type-c-service/src/wrapper/cfu.rs
@@ -0,0 +1,102 @@
+//! CFU message bridge
+//! TODO: remove this once we have a more generic FW update implementation
+use embedded_services::fw_update::{FwUpdate as FwUpdateTrait, Error as FwError};
+use embedded_services::cfu::*;
+use embedded_services::cfu::component::*;
+use embedded_cfu_protocol::protocol_definitions::*;
+use embedded_services::type_c::controller::Controller;
+use embedded_services::{debug, error};
+
+use super::*;
+
+impl<const N: usize, C: Controller + FwUpdateTrait> ControllerWrapper<'_, N, C> {
+    /// Process a GetFwVersion command
+    async fn process_get_fw_version(&self, target: &mut C) -> InternalResponseData {
+        let version = match target.get_active_fw_version().await {
+            Ok(v) => v,
+            Err(FwError::General(e)) => {
+                error!("Failed to get active firmware version: {:?}", e);
+                return InternalResponseData::ComponentBusy;
+            }
+            Err(FwError::Bus(_)) => {
+                error!("Failed to get active firmware version, bus error");
+                return InternalResponseData::ComponentBusy;
+            }
+        };
+
+
+        let dev_inf = FwVerComponentInfo::new(FwVersion::new(version), self.cfu_device.component_id());
+        let comp_info: [FwVerComponentInfo; MAX_CMPT_COUNT] = [dev_inf; MAX_CMPT_COUNT];
+        InternalResponseData::FwVersionResponse(GetFwVersionResponse {
+                    header: GetFwVersionResponseHeader::new(
+                        1,
+                        GetFwVerRespHeaderByte3::NoSpecialFlags,
+                    ),
+                    component_info: comp_info,
+                })
+    }
+
+    /// Process a CFU command
+    pub async fn process_cfu_command(&self, controller: &mut C, command: &RequestData) -> Option<InternalResponseData> {
+        match command {
+            RequestData::FwVersionRequest => {
+                debug!("Got FwVersionRequest");
+                Some(self.process_get_fw_version(controller).await)
+            }
+            RequestData::GiveOffer(offer) => {
+                debug!("Got GiveOffer");
+                // accept any and all offers regardless of what version it is
+                if offer.component_info.component_id == self.cfu_device.component_id() {
+                    debug!("Accepting offer");
+                    Some(InternalResponseData::OfferResponse(FwUpdateOfferResponse::new_accept(HostToken::Driver)))
+                } else {
+                    debug!("Rejecting offer, ID mimismatch");
+                    None
+                }
+            }
+            RequestData::GiveContent(content) => {
+               let data = &content.data[0..content.header.data_length as usize];
+               if content.header.flags & FW_UPDATE_FLAG_FIRST_BLOCK != 0 {
+                    debug!("Got first block");
+                    // Need to start the update
+                    if let Err(_) = controller.start_fw_update().await {
+                        error!("Failed to start FW update");
+                        Some(InternalResponseData::ContentResponse(FwUpdateContentResponse::new(content.header.sequence_num, CfuUpdateContentResponseStatus::ErrorPrepare)))
+                    } else {
+                        if let Err(_) = controller.write_fw_contents(content.header.firmware_address as usize, data).await {
+                            error!("Failed to write first block");
+                            Some(InternalResponseData::ContentResponse(FwUpdateContentResponse::new(content.header.sequence_num, CfuUpdateContentResponseStatus::ErrorWrite)))
+                        } else {
+                            Some(InternalResponseData::ContentResponse(FwUpdateContentResponse::new(content.header.sequence_num, CfuUpdateContentResponseStatus::Success)))
+                        }
+                    }
+               } else {
+                    debug!("Got last block");
+                     if let Err(_) = controller.write_fw_contents(content.header.firmware_address as usize, data).await {
+                            error!("Failed to write first block");
+                            Some(InternalResponseData::ContentResponse(FwUpdateContentResponse::new(content.header.sequence_num, CfuUpdateContentResponseStatus::ErrorWrite)))
+                        } else {
+                            Some(InternalResponseData::ContentResponse(FwUpdateContentResponse::new(content.header.sequence_num, CfuUpdateContentResponseStatus::Success)))
+                        }
+               }
+            }
+            RequestData::FinalizeUpdate => {
+                debug!("Got FinalizeUpdate");
+                if let Err(_) = controller.finalize_fw_update().await {
+                    error!("Failed to finalize FW update");
+                }
+                None
+            },
+            _ => {
+                debug!("Got other command: {:#?}", command);
+                None
+
+            }
+        }
+    }
+
+    /// Sends a CFU response to the command
+    pub async fn send_cfu_response(&self, response: Option<InternalResponseData>) {
+        self.cfu_device.send_response(response.unwrap_or(InternalResponseData::ComponentPrepared)).await;
+    }
+}
